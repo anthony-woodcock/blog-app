d68f8d5c21383a092002aa056bb46d643b235e00
Anthony Woodcock <anthonywoodcockmusic1991@hotmail.co.uk>
2017-12-30 16:13:33 +0000

    index page shows blog post from /restful_blog_app using forEach and body parser

--- app.js
+++ app.js
@@ -1,25 +1,40 @@
-var express = require('express')
+var express = require("express")
 var app = express()
 var bodyParser = require("body-parser")
 var mongoose = require("mongoose")
 
-// APP CONFIG
+
 mongoose.connect("mongodb://localhost/restful_blog_app")
-app.set("view engine", "ejs")
+app.set("view engine ", "ejs")
 app.use(express.static("public"))
 app.use(bodyParser.urlencoded({extended: true}))
 
-//MONGOOSE/MODEL CONFIG
-var blogSchema = new mongoose.Schema({
-    title: String,
-    image: String,
-    body: String,
-    created: {type: Date, default: Date.now}
-})
-var Blog = mongoose.model("Blog", blogSchema)
 
-//RESFUL ROUTES
+//Blog Schema
+var blogSchema = new mongoose.Schema({
+   title: String,
+   image: String,
+   body: String,
+   created: {type:Date, default: Date.now}
+})
+
+var Blog = mongoose.model("Blog",blogSchema);
+
+//Restful Routes
+app.get("/",function(req,res){
+   res.redirect("/blogs")
+})
+//INDEX ROUTE
+app.get("/blogs",function(req,res){
+    Blog.find({},function(err,blogs){
+       if(err){
+           console.log("Error")
+       } else{
+            res.render("index.ejs",{blogs: blogs})
+       }
+    })
+})
 
 app.listen(3000, function(){
-    console.log("Server is running on 3000")
+    console.log("Server is running");
 })
