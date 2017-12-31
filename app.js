7d7344eb179ccec6b93c73d8d0903fc7fa4ab6f0
Anthony Woodcock <anthonywoodcockmusic1991@hotmail.co.uk>
2017-12-31 12:13:37 +0000

    blog/id/edit will update the blog and redirect back to blog page. use method-override to create a put request on a html form.

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ var express = require("express")
 var app = express()
 var bodyParser = require("body-parser")
 var mongoose = require("mongoose")
+var methodOverride = require("method-override")
 
 
 mongoose.connect("mongodb://localhost/restful_blog_app")
 app.set("view engine ", "ejs")
 app.use(express.static("public"))
 app.use(bodyParser.urlencoded({extended: true}))
+app.use(methodOverride("_method"))
 
 
 //Blog Schema
@@ -64,6 +66,27 @@ app.get("/blogs/:id", function(req, res){
             res.redirect('/blogs')
         } else {
             res.render("show.ejs", {blog: foundBlog})
+        }
+    })
+})
+//EDIT ROUTE
+app.get("/blogs/:id/edit", function(req, res){
+    Blog.findById(req.params.id, function(err, foundBlog){
+        if(err){
+            res.redirect("/blogs")
+        } else {
+            res.render("edit.ejs",{blog: foundBlog})
+        }
+    })
+})
+
+//UPDTAE ROUTE
+app.put("/blogs/:id", function(req, res){
+    Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog){
+        if(err){
+            res.redirect("/blogs")
+        } else {
+            res.redirect("/blogs/" + req.params.id)
         }
     })
 })
